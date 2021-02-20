0023b1f7ce8401bf9a7a281761fd1571d506bedd
angelmedina2356 <58187924+angelmedina2356@users.noreply.github.com>
2021-02-20 00:47:39 -0600

    Add files via upload

---
+++ Actividad3_AJMR.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+/*:
+# Playground - Actividad 3
+* Tipos de datos
+* Asociación de tipos
+* Arreglos y Diccionarios
+*/
+
+
+/*: 
+### Actividad de Tipos de datos
+A) Declarar cuatro variables con tres diferentes tipos de datos, cada uno que corresponda a una numero entero, un numero decimal (flotante), una cadena de texto, realizando la asignación explicita y la asignación inferida
+*/
+var numeroentero:Int = 45
+var numerodecimal:Float = 45.5
+var cadenadetexto:String; cadenadetexto = "actividad"
+var cadenadetextoinferida = "hola"
+
+/*:
+### Asociación de tipos
+A) Declara el tipo de dato por asociación para un tipo de dato String
+*/
+var variable1:String = "hello"
+var vari1 = "hello"
+//: B) Declara el tipo de dato por asociación para un tipo de dato  Número entero.
+var variable2:Int = 45
+var vari2 = 45 
+/*: 
+### Arreglos y Diccionarios
+A) Crea la variable "numeros" de tipo Array con números consecutivos del 1 a 10.
+*/
+var numeros:Array<Int> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//y luego crea la variable diasSemana de tipo Dictionary con la relación numero:día Ej. 1:"Lunes”//
+
+var diasSemana:Dictionary = [1: "Lunes",
+                             2: "Martes",
+                             3: "Miercoles",
+                             4: "Jueves",
+                             5: "Viernes",
+                             6: "Sabado",
+                             7: "Domingo"
+                            ]
+    	
+
+
+
+
